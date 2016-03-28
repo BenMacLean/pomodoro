97bc169aeb1407214f2b0367de022be23e4d39dd
BenMacLean <benhmaclean@gmail.com>
2016-03-28 09:06:02 -0300

    gitignore

--- wp-content/plugins/beaver-builder-lite-version/includes/column-group.php
+++
@@ -1,6 +0,0 @@
-
-<div<?php FLBuilder::render_column_group_attributes( $group ); ?>>
-	<?php foreach ( $cols as $col ) : ?>
-		<?php FLBuilder::render_column( $col ); ?>
-	<?php endforeach; ?>
-</div>
