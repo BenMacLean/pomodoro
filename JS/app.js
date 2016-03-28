9bb75effd6b6da9b8a881bd4e0877c8c60f549ac
BenMacLean <benhmaclean@gmail.com>
2016-03-28 09:36:58 -0300

    fundamentals

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,27 @@
-
-
+var text, interval;
+//page initialization crap
 $(document).ready(function() {
   console.log("ready");
 
-var text=$("#timeRemaining")[0].innerText
+text=$("#timeRemaining")[0]
 console.log(text);
 
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
+$("#startButton").click(function() {
+  console.log("start");
+  text.innerText="start"
+  loop()
 
 })
+
+$("#stopButton").click(function() {
+  console.log("stop");
+  clearInterval(interval)
+})
+
+function loop() {
+  interval = setInterval(function() {
+    console.log("interval");
+  },1000)
+}
