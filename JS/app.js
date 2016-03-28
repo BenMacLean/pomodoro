c757cc73378cf47b8bb625968ed05950b6bd2f54
BenMacLean <benhmaclean@gmail.com>
2016-03-28 10:13:47 -0300

    09

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-var text, interval;
+var text, interval, counter=1500;
 //page initialization crap
 $(document).ready(function() {
   console.log("ready");
@@ -6,22 +6,55 @@ $(document).ready(function() {
 text=$("#timeRemaining")[0]
 console.log(text);
 
+
+
+
 })
 
+//loop start
 $("#startButton").click(function() {
   console.log("start");
-  text.innerText="start"
-  loop()
+  clearInterval(interval);
+  loop();
 
 })
 
-$("#stopButton").click(function() {
-  console.log("stop");
-  clearInterval(interval)
+//loop pause
+$("#pauseButton").click(function() {
+  console.log("pause");
+  clearInterval(interval);
+
 })
 
+//resume
+$("#resumeButton").click(function(){
+  clearInterval(interval);
+  loop();
+})
+
+//loop reset
+$("#resetButton").click(function() {
+  console.log("reset");
+  clearInterval(interval);
+  counter=1500;
+  text.innerText=convertToTime(counter);
+})
+
+//1 second loop
 function loop() {
   interval = setInterval(function() {
+    text.innerText=convertToTime(counter);
     console.log("interval");
+    counter--;
   },1000)
+}
+
+function convertToTime(counter) {
+var minutes = Math.floor(counter/60)
+var seconds = counter%60
+if (seconds.toString().length === 1) {
+  //add a zero before digit
+  seconds = "0" + seconds
+}
+  return minutes+ ":" + seconds
 }
